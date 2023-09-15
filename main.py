7ff8b439e8bf2a88804478061a3b3e68015453e8
James <sjkim.skw.laa@gmail.com>
2023-09-15 15:02:34 +0900

    edit main.py

--- main.py
+++ main.py
@@ -27,10 +27,7 @@ app.add_middleware(
 
 app.include_router(login.router)     
 app.include_router(users.router)
-app.include_router(metadata.router)
-app.include_router(projects.router)
-app.include_router(pages.router)
-app.include_router(voices.router)
+
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
